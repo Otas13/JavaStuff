bd42c4357646ac919ff214d0beb0d6d2f4e92db3
Otas13 <klapka.ota@gmail.com>
2016-11-09 23:21:02 +0100

    Fuuuuu

---
+++ Aligator/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static int getFuckingLowestValueIndex(int[] shit) {
+        int dirtyIndex = 0;
+        // nejvyssi cislo co jde nastavit do int aby se dal mohlo porovnavat jen <
+        int dirtyVal = Integer.MAX_VALUE;
+
+        // kdyz najde mensi cislo, ulozi index a hodnotu
+        for (int i = 0; i < shit.length; i++) {
+            if (shit[i] < dirtyVal) {
+                dirtyIndex = i;
+                dirtyVal = shit[i];
+            }
+        }
+        return dirtyIndex;
+    }
+
+    public static void main(String[] args)
+    {
+        // init pole
+        int[][] stuff = {
+                {4534, 76, 98345, 988, 123},
+                {272, 624, 843, 233, 4554},
+                {125, 268, 3567, 100, 76}
+        };
+
+        /*
+            tady se dela v podstate to same jako v te metode
+            muzes si s tim pohrat, rozbit ji na mensi kousky a pouzit je tu
+         */
+        int dirtyLowestSluttyValIndex = 0;
+        int sluttyLowestVal = Integer.MAX_VALUE;
+
+        for (int i = 0; i < stuff.length; i++){
+            int tmpWhoreIndex = getFuckingLowestValueIndex(stuff[i]);
+            if(stuff[i][tmpWhoreIndex] < sluttyLowestVal){
+                dirtyLowestSluttyValIndex = tmpWhoreIndex;
+                sluttyLowestVal = stuff[i][tmpWhoreIndex];
+            }
+        }
+
+        // tadaaa
+        System.out.println("Val: " + sluttyLowestVal + " Index: " + dirtyLowestSluttyValIndex);
+    }
+}
